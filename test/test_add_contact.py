79debe0751f46b6f3dd1f88201c4e0179fe5971e
ol6a <fedorova.o.a@bk.ru>
2020-09-28 23:13:30 +0300

    Выделен вспомогательный класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,11 +14,11 @@ def app(request):
     
 def test_add_contact(app):
     app.open_home_page()
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_contact(Contact(u"Алена", u"Николаевна", u"Пашкова", "alena", u"нет", u"АСТА",
                             u"Москва, ул. Мира, д.23-к.89", "902345", "89212346789", "902445", "789078",
                             "f1@moi-uni.ru", "f2@moi-uni.ru", "f3@moi-uni.ru", "https://moi-universitet.ru/", "12",
                             "August", "1989", "12", "August", "2019", u"Москва, ул. Красная, 34-23", "902345",
                             u"примечание"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
