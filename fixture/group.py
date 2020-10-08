90e32c8e97aaea67ce83dcab56bc86d880695fe8
ol6a <fedorova.o.a@bk.ru>
2020-10-08 22:15:43 +0300

    оптимизация ввода данных для формы группа

--- fixture/group.py
+++ fixture/group.py
@@ -18,15 +18,16 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_value, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_value).click()
+            wd.find_element_by_name(field_value).clear()
+            wd.find_element_by_name(field_value).send_keys(text)
 
     def open_groups_page(self):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,5 +2,16 @@ from model.group import Group
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="dfdf", header="dfdfdf", footer="dfdfdfdf"))
+    app.group.modify_first_group(Group(name="new", header="new1", footer="new2"))
+    app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New Group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New Header"))
     app.session.logout()
