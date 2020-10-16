d123166543d3221cb56f7e68b4b3255bcb287739
ol6a <fedorova.o.a@bk.ru>
2020-10-16 22:02:39 +0300

    Сравнение списков контактов при модификации контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,13 +4,16 @@ def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров", nickname="petr", title=u"-", company=u"АСТА",
+    contact=Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров", nickname="petr", title=u"-", company=u"АСТА",
                             address=u"Москва, ул. Мира, д.23-к.84", homephone="902345", mobilephone="89212346789", workphone="902445", fax="789078",
                             email="f4@moi-uni.ru", email2="f5@moi-uni.ru", email3="f6@moi-uni.ru", homepage="https://moi-universitet.ru/", bday="16",
                             bmonth="August", byear="1959", aday="12", amonth="August", ayear="2009", address2=u"Москва, ул. Красная, 34-23",
-                            phone2="902347", notes=u"test"))
+                            phone2="902347", notes=u"test")
+    contact.id=old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 #def test_modify_first_contact_company(app):
