4f8fe509fa457327f9eb1057cce74040a2bcdaa6
ol6a <fedorova.o.a@bk.ru>
2020-10-17 00:39:57 +0300

    Добавляем случайность в тест удаления групы

--- fixture/group.py
+++ fixture/group.py
@@ -38,10 +38,16 @@ class GroupHelper:
             wd.find_element_by_link_text("groups").click()
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def select_group_by_index(self, index):
+        wd=self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
