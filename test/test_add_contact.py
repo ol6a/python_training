17d7829f1f58c5de3738ac2a966fb02ee1ee2971
ol6a <fedorova.o.a@bk.ru>
2020-10-16 21:58:22 +0300

    удаление функции для вычисления ключа из теста

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,18 +5,13 @@ from sys import maxsize
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(u"Светлана", u"Николаевна", u"Медвееава", "alena", u"нет", u"АСТА",
-                            u"Москва, ул. Мира, д.23-к.89", "902345", "89212346789", "902445", "789078",
-                            "f1@moi-uni.ru", "f2@moi-uni.ru", "f3@moi-uni.ru", "https://moi-universitet.ru/", "12",
-                            "August", "1989", "12", "August", "2019", u"Москва, ул. Красная, 34-23", "902345",
-                            u"примечание")
+    contact = Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров", nickname="petr", title=u"-", company=u"АСТА",
+                            address=u"Москва, ул. Мира, д.23-к.84", homephone="902345", mobilephone="89212346789", workphone="902445", fax="789078",
+                            email="f4@moi-uni.ru", email2="f5@moi-uni.ru", email3="f6@moi-uni.ru", homepage="https://moi-universitet.ru/", bday="16",
+                            bmonth="August", byear="1959", aday="12", amonth="August", ayear="2009", address2=u"Москва, ул. Красная, 34-23",
+                            phone2="902347", notes=u"test")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
-    def id_or_max(cont):
-        if cont.id:
-            return int(cont.id)
-        else:
-            return maxsize
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key = Contact.id_or_max)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
