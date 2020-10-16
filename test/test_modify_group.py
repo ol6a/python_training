71715eb22c164ae08eceb5db79c6aa5230626909
ol6a <fedorova.o.a@bk.ru>
2020-10-16 21:42:24 +0300

    Сортировка списков перед сравнением (тест модификации групп)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,9 +4,13 @@ def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="new", header="new1", footer="new2"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
