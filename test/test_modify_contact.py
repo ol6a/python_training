d93b2fbfff38106fd607a4097db5262be0805ad4
ol6a <fedorova.o.a@bk.ru>
2020-10-16 19:46:27 +0300

    В тесты удаления/модификации групп и контактов добавлены проверки на количество элементов в списках

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,20 +3,23 @@ from model.contact import Contact
 def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров", nickname="petr", title=u"-", company=u"АСТА",
                             address=u"Москва, ул. Мира, д.23-к.84", homephone="902345", mobilephone="89212346789", workphone="902445", fax="789078",
                             email="f4@moi-uni.ru", email2="f5@moi-uni.ru", email3="f6@moi-uni.ru", homepage="https://moi-universitet.ru/", bday="16",
                             bmonth="August", byear="1959", aday="12", amonth="August", ayear="2009", address2=u"Москва, ул. Красная, 34-23",
                             phone2="902347", notes=u"test"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
-def test_modify_first_contact_company(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
-    app.contact.modify_first_contact(Contact(company=u"ИНФОРМ"))
+#def test_modify_first_contact_company(app):
+ #   if app.contact.count() == 0:
+ #       app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
+ #   app.contact.modify_first_contact(Contact(company=u"ИНФОРМ"))
 
 
-def test_modify_first_contact_birthday(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
-    app.contact.modify_first_contact(Contact(bday="22", bmonth="September", byear="1980"))
+#def test_modify_first_contact_birthday(app):
+   # if app.contact.count() == 0:
+   #     app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
+   # app.contact.modify_first_contact(Contact(bday="22", bmonth="September", byear="1980"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,17 +3,21 @@ from model.group import Group
 def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="new", header="new1", footer="new2"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 
-def test_modify_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(name="New Group"))
+
+#def test_modify_group_name(app):
+ #   if app.group.count() == 0:
+  #      app.group.create(Group(name="test"))
+  #  app.group.modify_first_group(Group(name="New Group"))
 
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    app.group.modify_first_group(Group(header="New Header"))
+#def test_modify_group_header(app):
+ #   if app.group.count() == 0:
+  #      app.group.create(Group(name="test"))
+  #  app.group.modify_first_group(Group(header="New Header"))
