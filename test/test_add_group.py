1cd712dd6965dcd72a08dfe6bd915a751cbd04b4
ol6a <fedorova.o.a@bk.ru>
2020-10-26 10:11:32 +0300

    временное использование фиксированных значений тестовых данных групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,17 +4,9 @@ from sys import maxsize
 import pytest
 import random
 import string
+from data.groups import constant as testdata
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata =[
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 
