5b96e5dd01e1113a0897f3ab4f2570b945846def
ol6a <fedorova.o.a@bk.ru>
2020-10-26 23:46:37 +0300

    Исправлена ошибка при загрузке данных групп из JSON файла

--- conftest.py
+++ conftest.py
@@ -38,10 +38,10 @@ def stop(request):
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
-            testdata=load_from_module(fixture[5:])
+            testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_")
-            testdata=load_from_json(fixture[5:])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata,ids=[str(x) for x in testdata])
 
 
