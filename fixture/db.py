759c8d8084b9ed31773331def359fa943c1657fe
ol6a <fedorova.o.a@bk.ru>
2020-11-02 22:44:42 +0300

    получение списка контактов из базы данных

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 class DbFixture:
     def __init__(self, host, name, user, password):
         self.host=host
@@ -19,6 +20,27 @@ class DbFixture:
         finally:
             cursor.close()
         return list
+
+
+    def get_contact_list(self):
+        list=[]
+        cursor=self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, middlename, lastname, nickname, company, title, address, home,mobile, "
+                           "work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, "
+                           "phone2, notes from addressbook")
+            for row in cursor:
+                (id, firstname, middlename, lastname, nickname, company, title, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2,phone2, notes) = row
+                list.append(Contact(id=str(id), firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname, company=company, title=title, address=address, homephone=home, mobilephone=mobile, workphone=work,
+                                    fax=fax, email=email, email2=email2, email3=email3, homepage=homepage, bday=bday, bmonth=bmonth, byear=byear, aday=aday, amonth=amonth, ayear=ayear, address2=address2, phone2=phone2,
+                                    notes=notes))
+        finally:
+            cursor.close()
+        return list
+
+
+
+
 
 
     def destroy(self):
