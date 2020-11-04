677613874238b19f7263b5ca638c12d18ae28e05
ol6a <fedorova.o.a@bk.ru>
2020-11-04 23:28:51 +0300

    Список контактов не входящих в группу с заданным id

--- check_db_connection.py
+++ check_db_connection.py
@@ -4,7 +4,7 @@ from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
-    l=db.get_contact_in_group(Group(id='205'))
+    l=db.get_contact_not_in_group(Group(id='205'))
     for item in l:
         print(item)
     print(len(l))
