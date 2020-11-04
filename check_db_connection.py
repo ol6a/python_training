3e72d59bf696008e900944250148d09a3e2d0f49
ol6a <fedorova.o.a@bk.ru>
2020-11-04 23:11:20 +0300

    Какие контакты в  группе с указанным id

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,10 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
-    l=db.get_contact_list()
+    l=db.get_contact_in_group(Group(id='205'))
     for item in l:
         print(item)
     print(len(l))
--- fixture/orm.py
+++ fixture/orm.py
@@ -13,6 +13,7 @@ class ORMFixture:
         name = Optional(str, column="group_name")
         header = Optional(str,column="group_header")
         footer = Optional(str, column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -41,11 +42,11 @@ class ORMFixture:
         address2 = Optional(str)
         phone2 = Optional(str)
         notes = Optional(str)
-
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
         deprecated = Optional(str, column='deprecated')
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.bind('mysql', host=host, database=name, user=user, password=password)
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -70,3 +71,8 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contact_in_group(self,group):
+        orm_group=list(select(g for g in ORMFixture.ORMGroup if g.id==group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
