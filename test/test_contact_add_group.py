cde42b7fe397ba50e6ac7e1d905931bd8d3f5058
ol6a <fedorova.o.a@bk.ru>
2020-11-06 23:05:29 +0300

    Тесты добавления контакта и удаления контакта из группы

--- test/test_contact_add_group.py
+++ test/test_contact_add_group.py
@@ -4,32 +4,28 @@ import random
 from fixture.orm import ORMFixture
 
 
-def test_contact_add_group(app, db):
-    if len(db.get_group_list()) == 0:
+def test_contact_add_group(app):
+    orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+    if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    #contacts = app.contact.get_contact_list()
-    #contact = random.choice(contacts)
-    db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
     groups=app.group.get_group_list()
     group=random.choice(groups)
-    if len(db.get_contact_not_in_group(group)) == 0 or len(db.get_contact_list()) == 0:
+    if len(orm.get_contact_not_in_group(group)) == 0 or len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
-        #contacts=sorted(db.get_contact_not_in_group(group), key=Contact.id_or_max)
-        contacts = sorted(db.get_contact_list(), key=Contact.id_or_max)
-        contact=contacts[0]
-    else:
-        contacts = db.get_contact_not_in_group(group)
-        contact = random.choice(contacts)
 
+    contacts = orm.get_contact_not_in_group(group)
+    contact = random.choice(contacts)
     app.contact.contact_add_group(contact.id, group.id)
-    #db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-    l = db.get_contact_in_group(group)
-    try:
-        for item in l:
-            if item == contact:
-                print("Контакт с id=" + str(contact.id) + " входит в группу с id=" + str(group.id))
-                return True
-    finally:
-        pass
+    new_contacts = orm.get_contact_in_group(group)
+    print("Контакт с id=" + str(contact.id) + " входит в группу с id=" + str(group.id))
+    assert contact in new_contacts
+   #l = db.get_contact_in_group(group)
+   #try:
+   #    for item in l:
+   #         if item == contact:
+   #             print("Контакт с id=" + str(contact.id) + " входит в группу с id=" + str(group.id))
+   #             return True
+  # finally:
+  #      pass
 
 
--- test/test_contact_remove_group.py
+++ test/test_contact_remove_group.py
@@ -3,31 +3,27 @@ from model.group import Group
 import random
 from fixture.orm import ORMFixture
 
-def test_contact_remove_group(app, db):
-    if len(db.get_contact_list()) == 0:
+def test_contact_remove_group(app):
+    orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+    if len(orm.get_contact_list()) == 0:
         app.contact.create(Contact(firstname=u"Петр", middlename=u"Петрович", lastname=u"Петров"))
-    if len(db.get_group_list()) == 0:
+    if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     groups=app.group.get_group_list()
     group=random.choice(groups)
-    db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-    if len(db.get_contact_in_group(group)) == 0:
+
+    if len(orm.get_contact_in_group(group)) == 0:
         contacts = app.contact.get_contact_list()
         contact = random.choice(contacts)
         app.contact.contact_add_group(contact.id, group.id)
         print("Контакт с id=" + str(contact.id) + " добавлен в группу с id=" + str(group.id))
     else:
-        contacts=db.get_contact_in_group(group)
+        contacts=orm.get_contact_in_group(group)
         contact=random.choice(contacts)
     app.contact.contact_del_group(contact, group)
+    print("Контакт с id=" + str(contact.id) + " удален из группы с id=" + str(group.id))
+    new_contacts = orm.get_contact_not_in_group(group)
+    assert contact in new_contacts
 
-    l = db.get_contact_not_in_group(group)
-    try:
-        for item in l:
-            if item == contact:
-                print("Контакт с id="+str(contact.id)+" удален из группы с id="+str(group.id))
-                return True
-    finally:
-        pass
 
 
